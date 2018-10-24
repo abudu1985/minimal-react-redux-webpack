df5ff8f4caf04ba76ab514e064bdffa2c437b7dd
igor <ig1985@ukr.net>
2018-10-24 21:42:06 +0300

    first commit

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -6,11 +6,11 @@ const App = () => (
   <div className="row mt-5">
     <div className="col-md-4 offset-md-1">
       <h2>Articles</h2>
-      <List />
+      <Form />
     </div>
     <div className="col-md-4 offset-md-1">
       <h2>Add a new article</h2>
-      <Form />
+      <List />
     </div>
   </div>
 );
